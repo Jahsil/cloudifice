f9038fe9584da421ca865e63b5a3cefc33e1f8bf
Eyouel Haile <eyouel2016@gmail.com>
2025-01-01 10:19:57 +0300

    add conifgurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,33 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xxs: "375px",
+        xs: "530px",
+        xxl: "1600px",
+      },
+      backgroundSize: {
+        // "300%": "300% 300%",
+      },
+      keyframes: {
+        // gradientAnimation: {
+        //   "0%": { backgroundPosition: "0% 50%" },
+        //   "50%": { backgroundPosition: "100% 50%" },
+        //   "100%": { backgroundPosition: "0% 50%" },
+        // },
+      },
+      animation: {
+        // gradientAnimation: "gradientAnimation 4s ease infinite",
+      },
+      colors: {
+        customGradientStart: "#dcd0ce", // Darker tone for #eee7e5
+        customGradientMid1: "#d2cddc", // Darker tone for #e7e4ee
+        customGradientMid2: "#c7d4d4", // Darker tone for #e1ecec
+        customGradientMid3: "#c8d4d4", // Darker tone for #e2ecec
+        customGradientEnd: "#d4d4d4", // Darker tone for #ededed
+      },
+    },
   },
   plugins: [],
 };
