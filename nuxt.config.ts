8a89074fc4f949db0fd97ce38d326c19a5a2dc2b
Eyouel Haile <eyouel2016@gmail.com>
2025-01-23 14:44:35 +0300

    set up public api base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:9000",
+    },
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
