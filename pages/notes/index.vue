c869f6c90d3dfb510be9ce26aa7214620cc77235
Eyouel Haile <eyouel2016@gmail.com>
2025-01-29 17:03:20 +0300

    add login functionality

--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -4,6 +4,25 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+// definePageMeta({
+//   middleware: "auth",
+// });
+import { useNuxtApp } from "#app";
+
+const { $axios } = useNuxtApp();
+
+async function getUser() {
+  try {
+    const response = await $axios.get("auth/user");
+  } catch (error) {
+    console.log("🚀 ~ getUser ~ error:", error);
+  }
+}
+
+onMounted(async () => {
+  await getUser();
+});
+</script>
 
 <style lang="scss" scoped></style>
