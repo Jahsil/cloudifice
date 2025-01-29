7c4c18b0e40b384fce63c45878d7b05ecd64a6c1
Eyouel Haile <eyouel2016@gmail.com>
2025-01-29 17:20:34 +0300

    disable server side rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:19000",
