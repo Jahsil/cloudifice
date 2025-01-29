9b7adb58d4ff63d8e5bb42705d0dff8e0b6067fc
Eyouel Haile <eyouel2016@gmail.com>
2025-01-29 17:01:58 +0300

    add pinia configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,11 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:9000",
+      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:19000",
     },
   },
+
+  modules: ["@pinia/nuxt"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
