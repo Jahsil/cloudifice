33fe05c4536185b86b5bd9bc17921f0e390d17b5
Eyouel Haile <eyouel2016@gmail.com>
2025-01-30 11:33:45 +0300

    update api path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   ssr: false,
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:19000",
+      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:8000",
     },
   },
 
