25dee4770103f24f0f0dcd33e9d6c7ebc76c8b69
Eyouel Haile <eyouel2016@gmail.com>
2025-02-10 10:37:52 +0300

    add pusher and laravel echo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  devServer: {
+    host: "localhost",
+    port: 3000,
+  },
   runtimeConfig: {
     public: {
       apiBase:
         process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api",
+      REVERB_APP_ID: process.env.NUXT_PUBLIC_REVERB_APP_ID,
+      REVERB_APP_KEY: process.env.NUXT_PUBLIC_REVERB_APP_KEY,
+      REVERB_APP_SECRET: process.env.NUXT_PUBLIC_REVERB_APP_SECRET,
+      REVERB_HOST: process.env.NUXT_PUBLIC_REVERB_HOST,
+      REVERB_PORT: process.env.NUXT_PUBLIC_REVERB_PORT,
+      REVERB_SCHEME: process.env.NUXT_PUBLIC_REVERB_SCHEME,
     },
   },
 
