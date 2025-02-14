9f791681c70227cb1235c35b89dc5eff8129ac7a
Eyouel Haile <eyouel2016@gmail.com>
2025-02-14 15:06:46 +0300

    dummy page

--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -1,28 +1,3 @@
-<template>
-  <div>
-    <p>Insights</p>
-  </div>
-</template>
+<script setup></script>
 
-<script setup>
-// definePageMeta({
-//   middleware: "auth",
-// });
-import { useNuxtApp } from "#app";
-
-const { $axios } = useNuxtApp();
-
-async function getUser() {
-  try {
-    const response = await $axios.get("auth/user");
-  } catch (error) {
-    console.log("🚀 ~ getUser ~ error:", error);
-  }
-}
-
-onMounted(async () => {
-  await getUser();
-});
-</script>
-
-<style lang="scss" scoped></style>
+<template></template>
