1200845ed8eefc876708d90d63db1690c7820675
Eyouel Haile <eyouel2016@gmail.com>
2025-02-19 16:51:07 +0300

    add production reverb info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,12 +12,19 @@ export default defineNuxtConfig({
         process.env.NUXT_LOCAL_API_BASE_URL,
       brodcastApiBase: process.env.NUXT_BROADCAST_API_BASE_URL,
 
-      REVERB_APP_ID: process.env.NUXT_PUBLIC_REVERB_APP_ID,
-      REVERB_APP_KEY: process.env.NUXT_PUBLIC_REVERB_APP_KEY,
-      REVERB_APP_SECRET: process.env.NUXT_PUBLIC_REVERB_APP_SECRET,
-      REVERB_HOST: process.env.NUXT_PUBLIC_REVERB_HOST,
-      REVERB_PORT: process.env.NUXT_PUBLIC_REVERB_PORT,
-      REVERB_SCHEME: process.env.NUXT_PUBLIC_REVERB_SCHEME,
+      REVERB_APP_ID:
+        process.env.NUXT_PUBLIC_REVERB_APP_ID || process.env.REVERB_APP_ID,
+      REVERB_APP_KEY:
+        process.env.NUXT_PUBLIC_REVERB_APP_KEY || process.env.REVERB_APP_KEY,
+      REVERB_APP_SECRET:
+        process.env.NUXT_PUBLIC_REVERB_APP_SECRET ||
+        process.env.REVERB_APP_SECRET,
+      REVERB_HOST:
+        process.env.NUXT_PUBLIC_REVERB_HOST || process.env.REVERB_SERVER_HOST,
+      REVERB_PORT:
+        process.env.NUXT_PUBLIC_REVERB_PORT || process.env.REVERB_SERVER_PORT,
+      REVERB_SCHEME:
+        process.env.NUXT_PUBLIC_REVERB_SCHEME || process.env.REVERB_SCHEME,
     },
   },
 
