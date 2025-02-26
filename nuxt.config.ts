72069a13c8afc0f17d0e51b81970201adb35559f
Eyouel Haile <eyouel2016@gmail.com>
2025-02-26 10:49:40 +0300

    use port 3000 as a dev server port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  // devServer: {
-  //   host: "0.0.0.0",
-  //   port: 3000,
-  // },
+  devServer: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   runtimeConfig: {
     public: {
       apiBase:
