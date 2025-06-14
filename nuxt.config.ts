5add246dcaf0808ef26dbb0951b64106e5100cf1
Eyouel Haile <eyouel2016@gmail.com>
2025-06-14 11:58:46 +0300

    run neccessary steps on deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
+  nitro: {
+    preset: 'static',
+  },
   devServer: {
     host: '0.0.0.0',
     port: 3000,
